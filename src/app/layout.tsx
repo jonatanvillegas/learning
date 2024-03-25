f73352dd392816e40543f40bfe2667bcdf0b6648
jonatan <villegasjoncena7@gmail.com>
2024-03-25 08:22:28 -0600

    configuracion del entorno mas su autenticacion

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import { cn } from '@/lib/utils'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Lexend } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+
+const lexend = Lexend({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Learning'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={cn(
+        lexend.className, 'antialiased min-h-screen pt-16'
+      )}>
+        <Navbar/>
+        {children}
+        </body>
+    </html>
+  )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Narbar = (props: Props) => {
+  return (
+    <div>Narbar</div>
+  )
+}
+
+export default Narbar
